6ef2b4ce1ba9f48669b8659d4297af23742a9670
Rods777 <rodelcuyag123@gmail.com>
2025-01-14 19:33:27 +0800

    Added Styling to NavBar Component

--- movie-app/src/components/NavBar.jsx
+++ movie-app/src/components/NavBar.jsx
@@ -1,11 +1,20 @@
 import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+import "../assets/css/NavBar.css"
 
 export function NavBar(){
     return (
         <nav className="navbar">
+            <input type="checkbox" id="check"/>
+            <label className="menu-bar-container" for="check">
+                <FaBars className="menu-bar"/>
+            </label>
+            
             <div className="navbar-brand">
                 {/* The Link component acts like <a><a/> */}
-                <Link to="/">Movie Vault</Link>
+                <Link to="/" className="navbar-brand-logo">
+                    <span className="navbar-logo-movie">Movie</span>Vault
+                </Link>
             </div>
             <div className="navbar-links">
                 <Link to="/" className="navbar-link">Home</Link>
