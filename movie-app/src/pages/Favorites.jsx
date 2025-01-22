c6eac846162be1ef1d4d198b997c40fb019ee735
Rods777 <rodelcuyag123@gmail.com>
2025-01-23 00:04:00 +0800

    Added Search Favorite Feature

--- movie-app/src/pages/Favorites.jsx
+++ movie-app/src/pages/Favorites.jsx
@@ -1,20 +1,37 @@
 import "../assets/css/Favorites.css";
+import { useState } from "react";
 import { useMovieContext } from "../contexts/MovieContext";
+import { FaSearch } from "react-icons/fa";
 import { MovieCard } from "../components/MovieCard";
 
 export function Favorites(){
     const { favorites } = useMovieContext();
+    const [searchQuery, setSearchQuery] = useState("");
 
     return (
         <>
              {/* Conditional Rendering whenever there is a favorites or none */}
-            {favorites.length > 0 ? ( 
+            {favorites.length > 0 ? (
                 <div className="favorites">
                     <h2>Favorites</h2>
+                    <form className="search-container">
+                        <div className="search-form">
+                            <FaSearch className="search-icon"/>
+                            <input
+                                type="text"
+                                className="search-input" 
+                                placeholder="Search for favorites..."
+                                value={searchQuery}
+                                onChange={(e) => setSearchQuery(e.target.value)}
+                            />
+                        </div>
+                    </form>
                     <div className="movies-container">
                         {/* Iterates and display all the favorites */}
                         {favorites.map((movie) => (
-                                // Displays all the favorite movies
+                                // Conditional Rendering for the search query  
+                                movie.title.toLowerCase().startsWith(searchQuery.toLowerCase()) &&
+                                // Displays the favorite movies
                                 <MovieCard movie={movie} key={movie.id}/>
                         ))}
                     </div>
